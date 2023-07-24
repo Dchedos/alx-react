aa6ff8ab5504f2b63f00290e8d5c3a564341985e
Nchedo Okongwu <nchedookongwu@yahoo.com>
2023-07-24 13:58:53 +0000

    Task 1. Installing packages - Learning how to use webpack with a config file

---
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -0,0 +1,23 @@
+// Import jQuery and lodash
+import $ from 'jquery';
+import debounce from 'lodash/debounce';
+
+// Function to update the click counter
+let count = 0;
+function updateCounter() {
+  count++;
+  $('#count').text(`${count} clicks on the button`);
+}
+
+// Add elements to the HTML
+$(document).ready(function() {
+  // Add paragraphs and button elements
+  $('body').append('<p>Holberton Dashboard</p>');
+  $('body').append('<p>Dashboard data for the students</p>');
+  $('body').append('<button id="clickButton">Click here to get started</button>');
+  $('body').append('<p id="count"></p>');
+  $('body').append('<p>Copyright - Holberton School</p>');
+
+  // Bind debounce function to click event
+  $('#clickButton').on('click', debounce(updateCounter, 500));
+});
